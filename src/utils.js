0c02da1775bda348b21d64291fba073542036a0e
Todd Albert <todd@bocacode.com>
2023-02-27 15:09:15 -0500

    getting and verifying token

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,22 @@
+import { initializeApp, cert } from "firebase-admin/app"
+import { getAuth } from "firebase-admin/auth"
+import service_account from "../service_account.json" assert { type: 'json' }
 
-
-
-
-export function getSecrets(req, res) {
+export async function getSecrets(req, res) {
+  // first check if they are sending a token
+  if(!req.headers || !req.headers.authorization) {
+    res.status(403).send({ message: 'Forbidden' })
+    return
+  }
+  // connect to firebase
+  initializeApp({
+    credential: cert(service_account)
+  })
+  // VALIDATE TOKEN
+  const decoded = await getAuth().verifyIdToken(req.headers.authorization)
+  if(!decoded) {
+    res.status(403).send({ message: 'Forbidden' })
+    return
+  }
   res.send({ message: 'The baby eagle sits in the nest at night.' })
 }
